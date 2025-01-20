59cb8b77b980aca15ee5f9bbdc43433eda97c002
zvshkin <alecsandr.kozlov.97@mail.ru>
2025-01-20 22:53:50 +0300

    Практическая Работа № 2

---
+++ PR-02/PR-02/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ВАРИАНТ № А12/Б15"
+                + "\n1. Даны три целых числа: A, B, C. Проверить истинность высказывания: «Каждое из чисел A, B, C положительное»."
+                + "\n2. Ввести пять различных ненулевых целых чисел. Найти произведение трех наибольших чисел."
+                + "\n3. Проверить истинность высказывания: \"Все цифры данного целого положительного четырехзначного числа различны\"."
+                + "\n4. Из пяти целых различных ненулевых положительных и отрицательных чисел найти самое наименьшее число."
+                + "\n5. Дан номер месяца N - целое положительное число в диапазоне от 1 до 12 (1 - январь, 2 - февраль, ..., 12 - декабрь). Вывести название соответствующего номера квартала (квартал – это четвертая часть года, один квартал = три месяца) (\"1 квартал\", \"2 квартал\", ..., \"4 квартал\").");
+
+            otdel();
+            Z01();
+            otdel();
+            Z02();
+        }
+
+        static void Z01()
+        {
+            Console.WriteLine("Задание 1.");
+
+        Start: // Метка для возврата
+            try
+            {
+                Console.WriteLine("Задайте целое число A:");
+                int A = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Задайте целое число B:");
+                int B = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Задайте целое число C:");
+                int C = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("\nПроверка на истинность высказывания: «Каждое из чисел A, B, C положительное»\n");
+
+                // Проверка на положительность чисел
+                if (A > 0 && B > 0 && C > 0)
+                {
+                    Console.WriteLine("Высказывание истинно: все числа положительны.");
+                }
+                else
+                {
+                    Console.WriteLine("Высказывание ложно: не все числа положительны.");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: введите корректное целое число.\n");
+                goto Start; // Возврат к метке Start
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Ошибка: введенное число слишком большое или слишком маленькое.\n");
+                goto Start;
+            }
+        }
+        
+        static void otdel()
+        {
+            Console.WriteLine("\n--------------------------------------------------------------------------\n");
+        }
+
+        static void Z02()
+        {
+            Console.WriteLine("Задание 2.");
+
+        Start:
+            try
+            {
+                Console.WriteLine("Задайте первое ненулевое целое число:");
+                int A = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Задайте второе ненулевое целое число:");
+                int B = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Задайте третье ненулевое целое число:");
+                int C = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Задайте четвёртое ненулевое целое число:");
+                int D = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Задайте пятое ненулевое целое число:");
+                int E = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("\nВычисление произведения трёх наибольших чисел.\n");
+
+                // Проверка на ненулевые числа
+                if (A == 0 || B == 0 || C == 0 || D == 0 || E == 0)
+                {
+                    Console.WriteLine("Ошибка: одно или несколько чисел равны нулю. Попробуйте снова.\n");
+                    goto Start;
+                }
+
+                // Находим три наибольших числа
+                int[] numbers = { A, B, C, D, E };
+                Array.Sort(numbers); // Сортировка массива чисел по возрастанию
+
+                int max1 = numbers[4];
+                int max2 = numbers[3];
+                int max3 = numbers[2];
+
+                int umnozhenie = max1 * max2 * max3;
+
+                Console.WriteLine("Три наибольших числа: {0}, {1}, {2}", max1, max2, max3);
+                Console.WriteLine("Их произведение: {0}", umnozhenie);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: введите корректное целое число.\n");
+                goto Start;
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Ошибка: введенное число слишком большое или слишком маленькое.\n");
+                goto Start;
+            }
+        }
+    }
+}
---
+++ PR-02/PR-02/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("PR-02")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PR-02")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("c0a13be7-b1ff-4eb8-a55b-5b34a6cfc8a1")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
