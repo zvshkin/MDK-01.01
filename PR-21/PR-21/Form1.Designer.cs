e3bc85f8cd941fff528e58c8a9d7285dcff13e11
zvshkin <67660240+zvshkin@users.noreply.github.com>
2025-09-02 23:02:34 +0300

    UPD

---
+++ PR-21/PR-21/Form1.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace PR_21
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.ChisloA_Z1 = new System.Windows.Forms.TextBox();
+            this.ChisloB_Z1 = new System.Windows.Forms.TextBox();
+            this.ChisloC_Z1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblResult1 = new System.Windows.Forms.Label();
+            this.lblResult2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ChisloN_Z2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblResult3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.ChisloN_Z3 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblResult5 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.ChisloC_Z5 = new System.Windows.Forms.TextBox();
+            this.ChisloB_Z5 = new System.Windows.Forms.TextBox();
+            this.ChisloA_Z5 = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.btnExportToWord = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            resources.ApplyResources(this.tabControl1, "tabControl1");
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.lblResult1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.ChisloC_Z1);
+            this.tabPage1.Controls.Add(this.ChisloB_Z1);
+            this.tabPage1.Controls.Add(this.ChisloA_Z1);
+            this.tabPage1.Controls.Add(this.label1);
+            resources.ApplyResources(this.tabPage1, "tabPage1");
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.lblResult2);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.ChisloN_Z2);
+            this.tabPage2.Controls.Add(this.label7);
+            resources.ApplyResources(this.tabPage2, "tabPage2");
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.lblResult3);
+            this.tabPage3.Controls.Add(this.button3);
+            this.tabPage3.Controls.Add(this.label8);
+            this.tabPage3.Controls.Add(this.ChisloN_Z3);
+            this.tabPage3.Controls.Add(this.label9);
+            resources.ApplyResources(this.tabPage3, "tabPage3");
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Controls.Add(this.btnExportToWord);
+            this.tabPage5.Controls.Add(this.lblResult5);
+            this.tabPage5.Controls.Add(this.button4);
+            this.tabPage5.Controls.Add(this.label11);
+            this.tabPage5.Controls.Add(this.label12);
+            this.tabPage5.Controls.Add(this.label13);
+            this.tabPage5.Controls.Add(this.ChisloC_Z5);
+            this.tabPage5.Controls.Add(this.ChisloB_Z5);
+            this.tabPage5.Controls.Add(this.ChisloA_Z5);
+            this.tabPage5.Controls.Add(this.label14);
+            resources.ApplyResources(this.tabPage5, "tabPage5");
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // ChisloA_Z1
+            // 
+            resources.ApplyResources(this.ChisloA_Z1, "ChisloA_Z1");
+            this.ChisloA_Z1.Name = "ChisloA_Z1";
+            // 
+            // ChisloB_Z1
+            // 
+            resources.ApplyResources(this.ChisloB_Z1, "ChisloB_Z1");
+            this.ChisloB_Z1.Name = "ChisloB_Z1";
+            // 
+            // ChisloC_Z1
+            // 
+            resources.ApplyResources(this.ChisloC_Z1, "ChisloC_Z1");
+            this.ChisloC_Z1.Name = "ChisloC_Z1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblResult1
+            // 
+            resources.ApplyResources(this.lblResult1, "lblResult1");
+            this.lblResult1.Name = "lblResult1";
+            // 
+            // lblResult2
+            // 
+            resources.ApplyResources(this.lblResult2, "lblResult2");
+            this.lblResult2.Name = "lblResult2";
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // ChisloN_Z2
+            // 
+            resources.ApplyResources(this.ChisloN_Z2, "ChisloN_Z2");
+            this.ChisloN_Z2.Name = "ChisloN_Z2";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // lblResult3
+            // 
+            resources.ApplyResources(this.lblResult3, "lblResult3");
+            this.lblResult3.Name = "lblResult3";
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // ChisloN_Z3
+            // 
+            resources.ApplyResources(this.ChisloN_Z3, "ChisloN_Z3");
+            this.ChisloN_Z3.Name = "ChisloN_Z3";
+            this.ChisloN_Z3.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
+            // lblResult5
+            // 
+            resources.ApplyResources(this.lblResult5, "lblResult5");
+            this.lblResult5.Name = "lblResult5";
+            // 
+            // button4
+            // 
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label11
+            // 
+            resources.ApplyResources(this.label11, "label11");
+            this.label11.Name = "label11";
+            // 
+            // label12
+            // 
+            resources.ApplyResources(this.label12, "label12");
+            this.label12.Name = "label12";
+            // 
+            // label13
+            // 
+            resources.ApplyResources(this.label13, "label13");
+            this.label13.Name = "label13";
+            // 
+            // ChisloC_Z5
+            // 
+            resources.ApplyResources(this.ChisloC_Z5, "ChisloC_Z5");
+            this.ChisloC_Z5.Name = "ChisloC_Z5";
+            // 
+            // ChisloB_Z5
+            // 
+            resources.ApplyResources(this.ChisloB_Z5, "ChisloB_Z5");
+            this.ChisloB_Z5.Name = "ChisloB_Z5";
+            // 
+            // ChisloA_Z5
+            // 
+            resources.ApplyResources(this.ChisloA_Z5, "ChisloA_Z5");
+            this.ChisloA_Z5.Name = "ChisloA_Z5";
+            // 
+            // label14
+            // 
+            resources.ApplyResources(this.label14, "label14");
+            this.label14.Name = "label14";
+            // 
+            // btnExportToWord
+            // 
+            resources.ApplyResources(this.btnExportToWord, "btnExportToWord");
+            this.btnExportToWord.Name = "btnExportToWord";
+            this.btnExportToWord.UseVisualStyleBackColor = true;
+            this.btnExportToWord.Click += new System.EventHandler(this.btnExportToWord_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Controls.Add(this.tabControl1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.tabPage5.ResumeLayout(false);
+            this.tabPage5.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.TextBox ChisloC_Z1;
+        private System.Windows.Forms.TextBox ChisloB_Z1;
+        private System.Windows.Forms.TextBox ChisloA_Z1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblResult1;
+        private System.Windows.Forms.Label lblResult2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox ChisloN_Z2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblResult3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox ChisloN_Z3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnExportToWord;
+        private System.Windows.Forms.Label lblResult5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox ChisloC_Z5;
+        private System.Windows.Forms.TextBox ChisloB_Z5;
+        private System.Windows.Forms.TextBox ChisloA_Z5;
+        private System.Windows.Forms.Label label14;
+    }
+}
+
---
+++ PR-21/PR-21/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Word = Microsoft.Office.Interop.Word;
+
+namespace PR_21
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(ChisloA_Z1.Text, out int A) ||
+                !int.TryParse(ChisloB_Z1.Text, out int B) ||
+                !int.TryParse(ChisloC_Z5.Text, out int C))
+            {
+                lblResult1.Text = "Ошибка ввода!";
+                return;
+            }
+
+            if (A <= 0 || B <= 0 || C <= 0)
+            {
+                lblResult1.Text = "Числа должны быть положительными!";
+                return;
+            }
+
+            bool hasPair = (A == B) || (B == C) || (A == C);
+            lblResult1.Text = hasPair ? "Есть совпадающие числа." : "Нет совпадающих чисел.";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(ChisloN_Z2.Text, out int N) || N <= 0)
+            {
+                lblResult2.Text = "Ошибка ввода! Введите положительное число.";
+                return;
+            }
+
+            string numStr = N.ToString();
+            if (numStr.Length != 5)
+            {
+                lblResult2.Text = "Ошибка: число должно быть пятизначным.";
+                return;
+            }
+
+            int sum = 0;
+            int product = 1;
+            bool hasEven = false;
+
+            while (N > 0)
+            {
+                int digit = N % 10;
+                sum += digit;
+                if (digit % 2 == 0 && digit != 0)
+                {
+                    product *= digit;
+                    hasEven = true;
+                }
+                N /= 10;
+            }
+
+            if (!hasEven) product = 0;
+
+            int difference = sum - product;
+            lblResult2.Text = $"Разница: {difference}";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(ChisloN_Z3.Text, out int bytes) || bytes <= 0)
+            {
+                lblResult3.Text = "Ошибка ввода! Введите положительное число.";
+                return;
+            }
+
+            string numStr = bytes.ToString();
+            if (numStr.Length != 6)
+            {
+                lblResult3.Text = "Ошибка: число должно быть шестизначным.";
+                return;
+            }
+
+            int kilobytes = bytes / 1024;
+            int megabytes = kilobytes / 1024;
+
+            lblResult3.Text = $"Файл размером {bytes} байт" +
+                              $" | {kilobytes} Кб (полных)" +
+                              $" | {megabytes} Мб (полных)";
+        }
+
+        private int Max3(int A, int B, int C)
+        {
+            return Math.Max(A, Math.Max(B, C));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(ChisloA_Z5.Text, out int A) ||
+                !int.TryParse(ChisloB_Z5.Text, out int B) ||
+                !int.TryParse(ChisloC_Z5.Text, out int C))
+            {
+                lblResult5.Text = "Ошибка ввода!";
+                return;
+            }
+
+            int max = Max3(A, B, C);
+            lblResult5.Text = $"Максимальное: {max}";
+        }
+
+        private void AddHeading(Word.Document doc, string text)
+        {
+            object missing = System.Reflection.Missing.Value;
+            Word.Paragraph para = doc.Content.Paragraphs.Add(ref missing);
+            para.Range.Text = text;
+            para.Alignment = Word.WdParagraphAlignment.wdAlignParagraphJustify;
+            para.Range.Font.Size = 18;
+            para.Range.Font.Bold = 1;
+            para.Range.InsertParagraphAfter();
+            para.CloseUp();
+        }
+
+        private void AddText(Word.Document doc, string text)
+        {
+            object missing = System.Reflection.Missing.Value;
+            Word.Paragraph para = doc.Content.Paragraphs.Add(ref missing);
+            para.Range.Text = text;
+            para.Alignment = Word.WdParagraphAlignment.wdAlignParagraphJustify;
+            para.Range.Font.Size = 14;
+            para.Range.Font.Bold = 0;
+            para.Range.InsertParagraphAfter();
+            para.CloseUp();
+        }
+
+        private void InsertPageBreak(Word.Document doc)
+        {
+            object missing = System.Reflection.Missing.Value;
+            object pageBreak = Word.WdBreakType.wdPageBreak;
+            Word.Paragraph para = doc.Content.Paragraphs.Add(ref missing);
+            para.Range.Collapse(Word.WdCollapseDirection.wdCollapseEnd);
+            para.Range.InsertBreak(ref pageBreak);
+            para.Range.InsertParagraphAfter();
+            para.CloseUp();
+        }
+
+        private void btnExportToWord_Click(object sender, EventArgs e)
+        {
+            object missing = System.Reflection.Missing.Value;
+            Word.Application wordApp = null;
+            Word.Document wordDoc = null;
+
+            try
+            {
+                wordApp = new Word.Application();
+                wordDoc = wordApp.Documents.Add(ref missing, ref missing, ref missing, ref missing);
+
+                wordDoc.PageSetup.Orientation = Word.WdOrientation.wdOrientPortrait;
+                wordDoc.PageSetup.TopMargin = wordApp.InchesToPoints(0.60f);
+                wordDoc.PageSetup.BottomMargin = wordApp.InchesToPoints(0.60f);
+                wordDoc.PageSetup.LeftMargin = wordApp.InchesToPoints(0.80f);
+                wordDoc.PageSetup.RightMargin = wordApp.InchesToPoints(0.59f);
+
+                wordApp.Visible = true;
+
+                wordApp.ActiveWindow.Selection.ParagraphFormat.LineSpacingRule = Word.WdLineSpacing.wdLineSpaceSingle;
+                wordApp.ActiveWindow.Selection.ParagraphFormat.SpaceAfter = 0f;
+
+                Word.Paragraph title1 = wordDoc.Content.Paragraphs.Add(ref missing);
+                title1.Range.Text = "ОТЧЁТ ПО ПРАКТИЧЕСКОЙ РАБОТЕ";
+                title1.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                title1.Range.Font.Size = 26;
+                title1.Range.Font.Bold = 1;
+                title1.Range.InsertParagraphAfter();
+                title1.CloseUp();
+
+                Word.Paragraph title2 = wordDoc.Content.Paragraphs.Add(ref missing);
+                title2.Range.Text = "по МДК 01.01: Разработка программных модулей";
+                title2.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                title2.Range.Font.Size = 18;
+                title2.Range.Font.Bold = 1;
+                title2.Range.InsertParagraphAfter();
+                title2.CloseUp();
+
+                Word.Paragraph title3 = wordDoc.Content.Paragraphs.Add(ref missing);
+                title3.Range.Text = "\nВыполнил: ---\nГруппа: ИС-23Б\n";
+                title3.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                title3.Range.Font.Size = 16;
+                title3.Range.InsertParagraphAfter();
+                title3.CloseUp();
+
+                InsertPageBreak(wordDoc);
+
+                AddHeading(wordDoc, "Задание 1.");
+                AddText(wordDoc, "Проверить истинность высказывания: «Среди трех данных целых положительных чисел, введенных с клавиатуры, есть хотя бы одна пара совпадающих».");
+
+                string input1 = $"Введены числа: A = {ChisloA_Z1.Text}, B = {ChisloB_Z1.Text}, C = {ChisloC_Z5.Text}";
+                AddText(wordDoc, input1);
+
+                string result1 = lblResult1.Text;
+                AddText(wordDoc, $"Результат: {result1}");
+
+                AddHeading(wordDoc, "Задание 2.");
+                AddText(wordDoc, "Дано целое положительное пятизначное число N (N > 0). Найти разницу между суммой всех его цифр и произведением четных цифр.");
+
+                string input2 = $"Введено число N: {ChisloN_Z2.Text}";
+                AddText(wordDoc, input2);
+
+                string result2 = lblResult2.Text;
+                AddText(wordDoc, $"Результат: {result2}");
+
+                AddHeading(wordDoc, "Задание 3.");
+                AddText(wordDoc, "Дан размер некоторого файла в байтах (целочисленное положительное ненулевое шестизначное число). Используя операцию деления нацело, найти количество полных килобайтов и мегабайтов, которые занимает данный файл (1 килобайт = 1024 байта, 1 мегабайт = 1024 килобайта).");
+
+                string input3 = $"Размер файла: {ChisloN_Z3.Text} байт";
+                AddText(wordDoc, input3);
+
+                string result3 = lblResult3.Text;
+                AddText(wordDoc, $"Результат: {result3}");
+
+                AddHeading(wordDoc, "Задание 5.");
+                AddText(wordDoc, "Написать функцию int Max3(A, B, C) целого типа, возвращающую одно максимальное значение из 3-х своих аргументов (параметры A, B, C — целые числа).");
+
+                string input5 = $"Введены числа: A = {ChisloA_Z5.Text}, B = {ChisloB_Z5.Text}, C = {ChisloC_Z5.Text}";
+                AddText(wordDoc, input5);
+
+                string result5 = lblResult5.Text;
+                AddText(wordDoc, $"Результат: {result5}");
+
+                object filename = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) +
+                                  $"\\Отчёт-МДК-CSharp-ПР21.doc";
+
+                wordDoc.SaveAs2(ref filename);
+                MessageBox.Show($"Отчёт сохранён:\n{filename}", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при создании Word-документа:\n{ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
