83c71a023e2e66e44b947616c2fc8701a3d3b534
Kopnin <n.kopnin1337@mail.ru>
2025-10-07 22:10:23 +0300

    UPD

---
+++ PR-24/ClassLibrary1/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class TaskSolutions
+    {
+        /// <summary>
+        /// Находим кол-во положительных чисел в наборе
+        /// </summary>
+        /// <param name="numbers">набор чисел</param>
+        /// <returns>кол-во положительных чисел</returns>
+        public static int CountPositiveNumbers(int[] numbers)
+        {
+            return numbers.Count(num => num > 0);
+        }
+
+        /// <summary>
+        /// сложение с нечетными числами максимальное четное число из массива
+        /// </summary>
+        /// <param name="array">массив целых чисел</param>
+        public static void AddMaxEvenToOdds(int[] array)
+        {
+            int maxEven = array.Where(x => x % 2 == 0).Max();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    array[i] += maxEven;
+                }
+            }
+        }
+
+        /// <summary>
+        /// нахождение количества элементов, расположенных перед первым минимальным элементом
+        /// </summary>
+        /// <param name="array">массив целых чисел</param>
+        /// <returns>количество элементов перед первым минимальным элементом</returns>
+        public static int CountBeforeMin(int[] array)
+        {
+            int minValue = array.Min();
+            int minIndex = Array.IndexOf(array, minValue);
+            return minIndex;
+        }
+
+        /// <summary>
+        /// получаем N-й цифры числа
+        /// </summary>
+        /// <param name="K">1 положительное число</param>
+        /// <param name="N">номер цифры</param>
+        /// <returns>возвращает N-ю цифру числа, если таковая существует, иначе -1.</returns>
+        public static int DigitN(int K, int N)
+        {
+            string numberStr = K.ToString();
+            if (N > numberStr.Length)
+                return -1;
+
+            return int.Parse(numberStr[numberStr.Length - N].ToString());
+        }
+    }
+}
---
+++ PR-24/PR-24/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace PR_24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // задание 1: ввод набора чисел и подсчет положительных
+            Console.WriteLine("Задача 1: Найти количество положительных чисел в наборе");
+            int[] numbers = GetIntArrayFromUser();
+            Console.WriteLine("Количество положительных чисел: " + TaskSolutions.CountPositiveNumbers(numbers));
+
+            // задание 2: ввод массива чисел и операция сложения с максимальным четным
+            Console.WriteLine("\nЗадача 2: Сложить с нечетными числами максимальное четное число из массива");
+            int[] array2 = GetIntArrayFromUser();
+            TaskSolutions.AddMaxEvenToOdds(array2);
+            Console.WriteLine("Новый массив: " + string.Join(" ", array2));
+
+            // задание 3: ввод массива чисел и подсчет элементов до первого минимального
+            Console.WriteLine("\nЗадача 3: Количество элементов перед первым минимальным элементом");
+            int[] array3 = GetIntArrayFromUser();
+            Console.WriteLine("Количество элементов перед первым минимальным: " + TaskSolutions.CountBeforeMin(array3));
+
+            // задание 4: ввод числа и получение N-й цифры
+            Console.WriteLine("\nЗадача 4: Получить N-ю цифру числа");
+            int K = GetIntFromUser("Введите число K: ");
+            int N = GetIntFromUser("Введите номер цифры N: ");
+            Console.WriteLine("N-я цифра числа: " + TaskSolutions.DigitN(K, N));
+        }
+
+        // метод для ввода целого числа с проверкой
+        static int GetIntFromUser(string prompt)
+        {
+            int result;
+            while (true)
+            {
+                Console.Write(prompt);
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out result))
+                {
+                    return result;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: введено некорректное число. Попробуйте снова.");
+                }
+            }
+        }
+
+        // метод для ввода массива целых чисел с проверкой
+        static int[] GetIntArrayFromUser()
+        {
+            while (true)
+            {
+                Console.Write("Введите числа через пробел: ");
+                string input = Console.ReadLine();
+                string[] parts = input.Split(' ');
+                try
+                {
+                    int[] array = Array.ConvertAll(parts, int.Parse);
+                    return array;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Ошибка: введены некорректные данные. Попробуйте снова.");
+                }
+            }
+        }
+    }
+}
